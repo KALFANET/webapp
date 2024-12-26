a273a15d4866f9b870401405eda2fe6f782ecdc5
KALFANET <104522435+KALFANET@users.noreply.github.com>
2024-12-26 18:59:54 +0200

    Update index.js

--- webapp/src/api/index.js
+++ webapp/src/api/index.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const usersRoutes = require('./routes/users');
 const fileRoutes = require('./routes/files');
+const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000; // שימוש ב־Render PORT או ברירת מחדל
+const PORT = process.env.PORT || 5000;
 
 // שימוש ב-CORS וב-body-parser
 app.use(cors());
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 // חיבור נתיבים
 app.use('/api/users', usersRoutes);
 app.use('/api/files', fileRoutes);
+app.use('/auth', authRoutes);
 
 // נתיב לבדיקת סטטוס השרת
 app.get('/api/status', (req, res) => {
